99f628a97c76e822a0266f3728ce8065df997de8
Veronfc <veronfc1701@gmail.com>
2023-09-11 14:24:27 +0200

    broken check_winner function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameboard = document.getElementById('board')
 
 const game = () => {
-  const board = ['X','','','X','','','X','','']
+  const board = ['','','','','','','','','']
   const players = []
 
   const player = (marker) => {
@@ -33,19 +33,35 @@ const game = () => {
 
     for (let i = 0; i < board.length; i++) {
       let mark = board[i].marker
-      if (i == 0) {
-        if (mark == board[1].marker && mark == board[2].marker ||
-            mark == board[3].marker && mark == board[6].marker ||
-            mark == board[4].marker && mark == board[8].marker) {
+      if (mark != '') {
+        if (i == 0 && (mark == board[1].marker && mark == board[2].marker ||
+          mark == board[3].marker && mark == board[6].marker ||
+          mark == board[4].marker && mark == board[8].marker)) {
+          win = true
+          break
+        } 
+        else if (i == 1 && (mark == board[4].marker && mark == board[7].marker)) {
           win = true
           break
         }
-      } 
-      else if (i == 1 && (mark == board[4].marker && mark == board[7].marker)) {
-        win = true
-        break
+        else if (i == 2 && (mark == board[4].marker && mark == board[6].marker || mark == board[5].marker && mark == board[8].marker)) {
+          win = true
+          break
+        }
+        else if (i == 3 && (mark == board[4].marker && mark == board[5].marker)) {
+          win = true
+          break
+        }
+        else if (i == 6 && (mark == board[7].marker && mark == board[8].marker)) {
+          win = true
+          break
+        }
+        else {win = false}
       }
-      else if (i == 2 && (mark))
+    }
+
+    if (win) {
+      alert('you won')
     }
   }
 
